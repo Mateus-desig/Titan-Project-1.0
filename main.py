45ecf16d546e147553580c460f6278b29a6cf25e
Mateus gonzaga <utracraft900@gmail.com>
2023-10-22 17:46:48 -0300

    Iniciando comandos

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import pyaudio
 import pyttsx3
 import json
+import core
 
 # Síntese de fala.
 engine = pyttsx3.init()
@@ -17,8 +18,7 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-
-
+# Reconhecimento de fala.
 model = Model('model')
 rec = KaldiRecognizer(model, 16000)
 
@@ -26,6 +26,7 @@ p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
 stream.start_stream()
 
+# Loop do reconhecimento de fala.
 while True:
     data = stream.read(8192)
     if len(data) == 0:
@@ -36,5 +37,9 @@ while True:
 
         if result is not None:
             text = result['text']
+
             print(text)
-            speak(text)
+            # speak(text)
+
+            if text == 'que horas são' or text == 'me diga as horas':
+                speak(core.SystemInfo.get_time())
